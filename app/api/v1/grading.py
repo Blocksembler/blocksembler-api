503577169221daaba2ddc006e10f64466152e9ba
Florian Wörister <florian.woerister@univie.ac.at>
2025-10-08 11:08:15 +0200

    create a response queue when scheduling a new grading job

--- app/api/v1/grading.py
+++ app/api/v1/grading.py
@@ -10,7 +10,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.schema.grading import ExerciseSubmission
-from app.config import MESSAGE_QUEUE_URL, MESSAGE_QUEUE_PASSWORD, MESSAGE_QUEUE_USER
+from app.config import MESSAGE_QUEUE_URL, MESSAGE_QUEUE_PASSWORD, MESSAGE_QUEUE_USER, GRADING_RESPONSE_QUEUE_TTL
 from app.db.database import get_session
 from app.db.model.exercise import ExerciseProgress
 from app.db.model.grading import GradingJob
@@ -19,6 +19,25 @@ router = APIRouter(
     prefix="/submission",
     tags=["submission"],
 )
+
+
+async def submit_grading_job(job_msg: dict, session: AsyncSession):
+    with amqp.Connection(MESSAGE_QUEUE_URL, userid=MESSAGE_QUEUE_USER, password=MESSAGE_QUEUE_PASSWORD) as c:
+        ch = c.channel()
+
+        ch.queue_declare(queue=f'grading_response.{job_msg["job_id"]}', durable=True, arguments={
+            "x-expires": GRADING_RESPONSE_QUEUE_TTL,
+        })
+
+        session.add(GradingJob(
+            id=job_msg["job_id"],
+            tan_code=job_msg["tan_code"],
+            exercise_id=job_msg["exercise_id"],
+            status="pending",
+            started=datetime.datetime.now()
+        ))
+
+        ch.basic_publish(amqp.Message(json.dumps(job_msg)), routing_key='grading_jobs')
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=str)
@@ -30,32 +49,22 @@ async def create_submission(new_submission: ExerciseSubmission, session: AsyncSe
     exercise_progress = result.scalars().first()
 
     if not exercise_progress:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Tried to submit for non-active exercise")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Tried to submit to an inactive exercise.")
 
     try:
-        with amqp.Connection(MESSAGE_QUEUE_URL, userid=MESSAGE_QUEUE_USER, password=MESSAGE_QUEUE_PASSWORD) as c:
-            job_msg = {
-                "job_id": str(uuid4()),
-                "exercise_id": new_submission.exercise_id,
-                "tan_code": new_submission.tan_code,
-                "solution_code": new_submission.solution_code
-            }
+        job_msg = {
+            "job_id": str(uuid4()),
+            "exercise_id": new_submission.exercise_id,
+            "tan_code": new_submission.tan_code,
+            "solution_code": new_submission.solution_code
+        }
 
-            session.add(GradingJob(
-                id=job_msg["job_id"],
-                tan_code=job_msg["tan_code"],
-                exercise_id=job_msg["exercise_id"],
-                status="pending",
-                started=datetime.datetime.now()
-            ))
+        await submit_grading_job(job_msg, session)
+        await session.commit()
 
-            ch = c.channel()
-            ch.basic_publish(amqp.Message(json.dumps(job_msg)), routing_key='grading_jobs')
-
-            await session.commit()
-            return job_msg["job_id"]
+        return job_msg["job_id"]
 
     except Exception as e:
         logging.error(e)
         await session.rollback()
-        raise HTTPException(status_code=500, detail="Scheduling grading job failed.")
+        raise HTTPException(status_code=500, detail="Scheduling a grading job failed.")
