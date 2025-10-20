0da0be8a0deb236c2c3f3bad4e763638c420432c
Florian Wörister <31357619+fwoerister@users.noreply.github.com>
2025-10-20 14:02:11 +0200

    Fw/rename grading to submission (#14)
    * rename "submissions" to "grading-jobs"
    
    * clean up env variables
    
    ---------
    
    Co-authored-by: Florian Wörister <florian.woerister@univie.ac.at>

--- app/mq/message_queue.py
+++ app/mq/message_queue.py
@@ -1,9 +1,15 @@
+import logging
+
 import aio_pika
 from aio_pika.abc import AbstractRobustChannel
 
+from app.config import MQ_USER, MQ_PWD, MQ_PORT, MQ_URL
+
 
 async def get_mq_channel() -> AbstractRobustChannel:
-    connection = await aio_pika.connect_robust("amqp://blocksembler:blocksembler@localhost:5672")
+    uri = f"amqp://{MQ_USER}:{MQ_PWD}@{MQ_URL}:{MQ_PORT}"
+    logging.debug('connect to: ', uri)
+    connection = await aio_pika.connect_robust(uri)
 
     async with connection:
         channel = await connection.channel()
--- tests/test_grading_job.py
+++ tests/test_grading_job.py
@@ -6,6 +6,7 @@ from fastapi import status
 from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
+from app.config import GRADING_JOB_ROUTING_KEY
 from app.db.database import get_session
 from app.main import app
 from app.mq.message_queue import get_mq_channel
@@ -19,7 +20,8 @@ def setup_mocks():
     channel_mock.get_exchange = AsyncMock(return_value=exchange_mock)
     return channel_mock, exchange_mock
 
-class TestSubmission:
+
+class TestGradingJob:
     def setup_method(self):
         self.engine = create_async_engine(DB_URI, echo=True, future=True)
         self.async_session = async_sessionmaker(self.engine, expire_on_commit=False, class_=AsyncSession)
@@ -38,18 +40,18 @@ class TestSubmission:
 
         client = TestClient(app)
 
-        submission = {
+        exercise_submission = {
             "tan_code": "test-tan-1",
             "exercise_id": 2,
             "solution_code": "addi r0 r0 r0"
         }
 
-        response = client.post("/submissions", json=submission)
+        response = client.post("/grading-jobs", json=exercise_submission)
 
         print(response.json())
 
         assert response.status_code == status.HTTP_201_CREATED
-        exchange_mock.publish.assert_awaited_once_with(ANY, routing_key='grading_jobs')
+        exchange_mock.publish.assert_awaited_once_with(ANY, routing_key=GRADING_JOB_ROUTING_KEY)
 
     def test_post_invalid_submission(self):
         channel_mock, exchange_mock = setup_mocks()
@@ -62,13 +64,13 @@ class TestSubmission:
 
         client = TestClient(app)
 
-        submission = {
+        exercise_submission = {
             "tan_code": "non-existing-tan",
             "exercise_id": 1,
             "solution_code": "addi r0 r0 r0"
         }
 
-        response = client.post("/submissions", json=submission)
+        response = client.post("/grading-jobs", json=exercise_submission)
 
         print(response.json())
 
